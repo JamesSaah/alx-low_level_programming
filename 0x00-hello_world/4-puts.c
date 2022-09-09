525867951632d458722569da9a239111d3f09fc0
James Saah Kollie <kolliejames118@gmail.com>
2022-09-09 13:43:35 -0700

    laugh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Writing
+ *
+ * Return: Always O (Fruitful)
+ */
+int main(void)
+{
+    puts("\ "Programming is like building a multilinggual
+puzzle ");
+    return(0);
+}    
