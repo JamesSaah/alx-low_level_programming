669fa4f2e0ad07415c3662f7b9bdeb3468fae662
James Saah Kollie <kolliejames118@gmail.com>
2022-09-09 13:58:02 -0700

    shout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Printf a text
+ *
+ * Return: Always 0 (Pass)
+ */
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of 
+art,\n" );
+	return (0)
+}
+
+
