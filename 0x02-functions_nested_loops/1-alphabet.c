68383a0a819659cdeeee83445163aace89fef831
James Saah Kollie <kolliejames118@gmail.com>
2022-09-14 19:38:33 -0700

    so

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+ /**
+ * print_alphabet - functions to print abc
+ *
+ * Return: 0
+ *
+ */
+
+Void print_alphabet(void)
+
+{
+Char c;
+
+For (c = 'a' c <='z'; c++)
+
+{
+
+_putchar(c);
+
+}
+_putchar('\n');
+
+}
