1eb80ffffce1c833c951d2820f286da4eef1ca96
James Saah Kollie <kolliejames118@gmail.com>
2022-09-15 01:33:30 -0700

    good

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,20 @@
 #ifndef HOLBERTON_H_INCLUDE
 #define  HOLBERTON_H_INCLUDE
 
-int _putchar(char);
-Void print_alphabet(void);
-Void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int c);
-int _abs(int);
-int print_last_digit(int);
-Void jack_bauer(void);
-Void times_table(void);
-int add(int a, int b);
-Void print_to_98(int n);
-Void print_times_table(int n);
-Void num(int r, int n);
+int _putchar(char),
+Void print_alphabet(void),
+Void print_alphabet_x10(void),
+int_islower(int c),
+int_isalpha(int c),
+int print_sign(int c),
+int _abs(int),
+int print_last_digit(int),
+Void jack_bauer(void),
+Void times_table(void),
+int add(int a, int b),
+Void print_to_98(int n),
+Void print_times_table(int n),
+Void num(int r, int n),
 
 #endif	
 
